6ef1f66e9789a58e89bb90a1ef95dce33eb6f059
l0o0gy <minamahone9620@gmail.com>
2024-02-09 01:08:47 +0300

    new Js H.W

---
+++ practis.js
@@ -0,0 +1,65 @@
+var age =18;
+if (age =20) {
+console.log('you are eligible for voting');
+}
+  else{
+    console.log('you are not eligible for voting');
+}
+var i=1;
+while (i<6) {
+console.log(i);
+    i++;
+    
+}
+for (var i=2; i<11; i++){
+    console.log(i,'1st loop');
+    for (var j=10; j>1; j--){
+      console.log(j,'second loop');
+    }
+}
+var day=5
+switch (day) {
+    case 1:
+      console.log('Sunday');  
+      break;
+
+      case 2:
+        console.log('Monady');  
+        break;
+
+      case 3:
+        console.log('Tuesday');  
+         break;
+
+      case 4:
+        console.log('Wednesday');  
+         break;
+
+      case 5:
+         console.log('Thursday');  
+          break;
+
+      case 6:
+         console.log('Friday'); 
+          break; 
+
+      case 7:
+         console.log('Saturday');  
+          break;
+
+}
+function sum(num1,num2) {
+    console.log(num1+num2);
+    console.log(num1*num2);    
+    console.log(num1/num2);
+    console.log(num1-num2);     
+}
+sum(6,19);
+
+var fruits=["Apple","Kiwi","Cherry","Straberry","Fig",]
+    console.log(fruits);
+
+
+
+
+
